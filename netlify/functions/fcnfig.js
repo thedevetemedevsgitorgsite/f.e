f87a00cf47d611e2a90c56cad8b41e94d967da34
Figsh <safetyunion5550@gmail.com>
2025-12-03 11:12:49 +0100

    Update fcnfig.js

--- netlify/functions/fcnfig.js
+++ netlify/functions/fcnfig.js
@@ -1,10 +1,2 @@
 // netlify/functions/fcnfig.js
-export async function handler() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      url: process.env.PUBLIC_SUPA_URL,
-      key: process.env.PUBLIC_SUPA_API
-    }),
-  };
-}
+export async function handler(){return{statusCode:200,body:JSON.stringify({url:process.env.PUBLIC_SUPA_URL,key:process.env.PUBLIC_SUPA_API})}}
