8f1c2715018f99ad529df170113a15ddd5ad9de6
Figsh <safetyunion5550@gmail.com>
2026-03-07 01:42:22 +0100

    Update fscss.js

--- assets/scripts/fscss.js
+++ assets/scripts/fscss.js
@@ -3,6 +3,6 @@ import {exec} from "https://cdn.jsdelivr.net/npm/fscss@1.1.15/e/xfscss.min.js";
 exec(
   {
     type: "URL", 
-    content: "/assets/styles/progress.fscss", 
+    content: "/assets/styles/progress.css", 
   }
 )
