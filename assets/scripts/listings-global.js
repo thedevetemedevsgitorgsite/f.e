dd30614f0ee7a65c602a0e42319ae8426954f5c7
Figsh <safetyunion5550@gmail.com>
2026-06-05 09:08:21 +0100

    Update listings-global.js

--- assets/scripts/listings-global.js
+++ assets/scripts/listings-global.js
@@ -1,1 +1,13 @@
 console.log("Hello from DevTemple")
+
+const uMention document.querySelectorAll(".user-mention");
+uMention.forEach(um => {
+  // 1. Apply Styles
+  um.style.cssText = `opacity:99%; text-decoration: 0.2px underline var(--glass-border); font-family: "ADLaM Display", monospace; cursor: pointer;`;
+
+  // 2. Find the matching profile
+  const mentionName = um.textContent.trim();
+  um.onclick = ()=>{
+    location.href = "https://devtem.org/s?s="+mentionName;
+  }
+});
